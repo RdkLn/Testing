80fd43b536467e0497cf6c345b93027230f5d9d2
radek <radlek19@gmail.com>
2021-10-29 13:18:54 +0200

    fab

--- app/src/main/java/com/example/testing/BlankFragment.kt
+++ app/src/main/java/com/example/testing/BlankFragment.kt
@@ -33,10 +33,17 @@ class BlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
+
+
+
+
+
+
+
+
 
     companion object {
         /**
